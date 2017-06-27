3dcb4989d12604e6e4c94e719ea013c043127960
MADHAV BAHL <madhavbahl10@gmail.com>
2017-06-27 22:04:44 +0530

    Partners page started and links updated

--- MAIN WEBSITE/link.js
+++ MAIN WEBSITE/link.js
@@ -1,7 +1,12 @@
 var bdy = document.getElementsByTagName("BODY")[0];
 /*bdy.addEventListener("mouseover",function(){
   bdy.style.cursor = "pointer";
-})
-bdy.addEventListener("click",function(){
-  location.href = "home.html";
 })*/
+
+
+function myfunc(){
+  location.href = "home.html";
+}
+function myFunction(){
+  setTimeout(myfunc, 1500);
+}
