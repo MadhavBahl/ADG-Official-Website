16938fcbd2b7098637dd884a7f68596a6ecb5671
MADHAV BAHL <madhavbahl10@gmail.com>
2017-07-09 00:43:52 +0530

    Projects Styles

--- MAIN WEBSITE/js/tryProject.js
+++ MAIN WEBSITE/js/tryProject.js
@@ -14,13 +14,26 @@ var scrollPos;
 var bottomPos;
 var bodyHeight = $("body").height()-$(window).height();
 var docWidth = document.documentElement.clientWidth;
+var docHeight = document.documentElement.clientHeight;
 var vwPos;
+var toVh = 100/heightDesktop;
+var toVw = 100/widthDesktop;
+var imgHeight = projectImage.clientHeight;
+var imgHeightInVh = imgHeight*toVh;
+var imgHeightInVw = imgHeight*toVw;
+var tempHt = 74;
 
 if(widthDesktop>500){
   iphone.style.width = "22vw";
   iphone.style.backgroundSize = "22vw";
   iphone.style.height = "80vh";
+  infoTwo.style.display = "none";
+  infoThree.style.display = "none";
   iphone.style.display = "inline-block";
+  //container.style.minHeight = "115vw";
+  $("#container").css({
+    "minHeight": tempHt + "vw"
+  });
   projectImage.style.marginTop = "6vw";
   projectImage.style.marginLeft = "3vw";
   projectImage.style.width = "16vw";
@@ -35,24 +48,53 @@ if(widthDesktop>500){
       bottomPos = vwPos + 27.4;
       var slowDown = vwPos*0.01;
 
+      if(vwPos<19){
+        tempHt=74;
+        container.style.transition = "1s";
+        container.style.backgroundColor = "rgb(33,33,33)";
+        infoOne.style.display = "block";
+        infoTwo.style.display = "none";
+        infoThree.style.display = "none";
+      }else if(vwPos<39){
 
-      disp.innerHTML = vwPos;
-      $("#coverProjects").css({
-          "clip": "rect(" + vwPos + "vw,16vw," + bottomPos + "vw,0vw)"
-      });
-      $("#coverProjects").css({
-          "marginTop": 6 - vwPos + "vw",
-      });
+        container.style.transition = "1s";
+        container.style.backgroundColor = "rgb(10,10,10)";
+        infoOne.style.display = "none";
+        infoTwo.style.display = "block";
+        infoTwo.style.marginTop = "33vw";
+        infoThree.style.display = "none";
+      }else{
+        container.style.transition = "1s";
+        container.style.backgroundColor = "rgb(33,33,33)";
+        infoOne.style.display = "none";
+        infoTwo.style.display = "none";
+        infoThree.style.marginTop = "66vw";
+        infoThree.style.display = "block";
+      }
+      if(bottomPos<(3*27.4)+8){
+        disp.innerHTML = bottomPos;
 
-  };
+          tempHt+=1.3;
+          $("#container").css({
+            "minHeight": tempHt + "vw"
+          });
 
+        $("#coverProjects").css({
+            "clip": "rect(" + vwPos + "vw,16vw," + bottomPos + "vw,0vw)"
+          });
+          $("#coverProjects").css({
+            "marginTop": 6 - vwPos + "vw",
+          });
+        }
+
+      };
 
 }else{
   iphone.style.width = "80vw";
   iphone.style.backgroundSize = "80vw";
   iphone.style.height = "80vh";
   infoOne.style.display = "none";
-  infoTwo.style.display = "none";
+  //infoTwo.style.display = "none";
   infoThree.style.display = "none";
   projectImage.style.marginTop = "23vw";
   projectImage.style.marginLeft = "12vw";
@@ -60,7 +102,7 @@ if(widthDesktop>500){
   projectImage.style.overflow = "hidden";
   projectImage.style.position = "absolute";
   projectImage.style.clip = "rect(0vw,56vw,97.4vw,0)";
-  container.style.height = "384vw";
+  container.style.minHeight = "350vw";
 
   window.onscroll = function() {
 
@@ -70,7 +112,7 @@ if(widthDesktop>500){
       var slowDown = vwPos*0.01;
 
 
-      disp.innerHTML = vwPos;
+      disp.innerHTML = imgHeight*toVh;
       $("#coverProjects").css({
           "clip": "rect(" + vwPos + "vw,56vw," + bottomPos + "vw,0vw)"
       });
@@ -78,6 +120,6 @@ if(widthDesktop>500){
           "marginTop": 23 - vwPos + "vw",
       });
 
-      if(vwPos)
+      //if(vwPos)
   };
 }
