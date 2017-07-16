a12f4e803dc3a5f91658abac46ea2d7632b9d5d5
MADHAV BAHL <madhavbahl10@gmail.com>
2017-07-16 22:09:32 +0530

    PARALLAX EFFECT CORRECTED

--- MAIN WEBSITE/js/proj.js
+++ MAIN WEBSITE/js/proj.js
@@ -4,25 +4,38 @@ projectImage.style.position = "absolute";
 projectImage.style.clip = "rect(0px,228px,410px,0)";
 var scrollPos;
 var bottomPos;
+var bottomPos2;
 var widthDesktop =  window.innerWidth;
 
 if(widthDesktop>520){
   window.onscroll = function() {
     scrollPos = $(window).scrollTop();
-    bottomPos = 410-0.647*scrollPos;
+    bottomPos = 0.647*scrollPos;
+    bottomPos2 = 410 - 0.647*scrollPos;
     //document.getElementById('disp').innerHTML=scrollPos;
     $("#freshers").css({
-        "clip": "rect(" + 0 + "px,228px," + bottomPos + "px,0px)"
+        "clip": "rect(" + 0 + "px,228px," + bottomPos2 + "px,0px)"
+    });
+    if(bottomPos<410){
+      $("#dodge").css({
+        "marginTop": bottomPos2-2
       });
+    }
   };
 }
 else{
   window.onscroll = function() {
     scrollPos = $(window).scrollTop();
-    bottomPos = 410-0.75*scrollPos;
+    bottomPos = 0.75*scrollPos;
+    bottomPos2 = 410-0.75*scrollPos;
     //document.getElementById('disp').innerHTML=bottomPos;
     $("#freshers").css({
-        "clip": "rect(" + 0 + "px,228px," + bottomPos + "px,0px)"
+        "clip": "rect(" + 0 + "px,228px," + bottomPos2 + "px,0px)"
+    });
+    if(bottomPos<410){
+      $("#dodge").css({
+        "marginTop": bottomPos2-4
       });
+    }
   };
 }
